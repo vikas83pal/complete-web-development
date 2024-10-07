ad4d7d0fe86be666d8f496ad5f6d99a64d4faf34
vikas83pal <vikas83pal@gmail.com>
2024-10-07 10:36:34 +0530

    es6 added

---
+++ ES6/script.js
@@ -0,0 +1,18 @@
+// let p = new Promise((resolve, reject) =>{
+//     let a = 1;
+//     if(a === 2){
+//         resolve("resolved");
+//         console.log("inside resolved promises");
+//     }else{
+//         reject("promises rejected");
+//         console.log("inside rejected promises");
+//     }
+// })
+
+// p.then((message) =>{
+//     console.log("inside then " + message);
+// }).catch((err) =>{
+//     console.log("inside catch " + err);
+// })
+
+
--- Exercise-11/script.js
+++
@@ -1,8 +0,0 @@
-let num = prompt("Enter the Number");
-
-let res = (num) =>{
-    if(num == 0 || num == 1) return 1;
-    else return num * res(num - 1);
-}
-
-alert("The Result is" + res(num));
